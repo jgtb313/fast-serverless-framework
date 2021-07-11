4271af9a846e0ddaff172f81ab098a70b86be792
João <joao@ecobonuz.com>
2021-07-11 01:27:15 -0300

    testing aws

--- src/state.js
+++ src/state.js
@@ -18,15 +18,19 @@ const state = {
     stage: ''
   },
   addTopic: function (value) {
+    console.log({ topic: value })
     this.topics = uniq([...this.topics, value], 'name')
   },
   addConsumer: function (value) {
+    console.log({ consumer: value })
     this.consumers = [...this.consumers, value]
   },
   addSchedule: function (value) {
+    console.log({ schedule: value })
     this.schedules = [...this.schedules, value]
   },
   addEndpoint: function (value) {
+    console.log({ endpoint: value })
     this.endpoints = [...this.endpoints, value]
   },
   setConfig: function () {
